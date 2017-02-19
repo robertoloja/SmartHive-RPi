7bff71d2afbfc7989111f90cccd227f19bcb3fb9
Roberto Loja <robertoloja@gmail.com>
2017-02-19 09:15:51 -0500

    Initial Node app setup.

---
+++ test.js
@@ -0,0 +1,16 @@
+// Import firebase
+var firebase = require("firebase");
+var firebaseui = require("firebaseui");
+
+// Initialize the DB
+var config = {
+    apiKey: "AIzaSyCghWvkizAwajHUizxQRjlU0GTvtYl7BuY",
+    authDomain: "smarthive-229a5.firebaseapp.com",
+    databaseURL: "https://smarthive-229a5.firebaseio.com",
+    storageBucket: "smarthive-229a5.appspot.com",
+    messagingSenderId: "386695389370"
+  };
+
+firebase.initializeApp(config);
+
+
